431a24ade1f56bf3c95219ece1740571bea80594
MaryKaya <sceran_biyoloji@hotmail.com>
2019-09-17 19:27:27 -0500

    It is updated by M.Kaya

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,12 +1,12 @@
 package Arrays;
 
-public class ReverseArray {
+public class ReverseArray{
     public static void main(String[] args) {
         System.out.println(reverseWord());
 
     }
     public static String reverseWord(){
-        String word= "ANNa";
+        String word= "aNNa";
         String reversed="";
         for (int i=word.length()-1; i>=0;i--){
             reversed+=word.charAt(i);
