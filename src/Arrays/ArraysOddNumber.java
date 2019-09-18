fd5272d6dc12e4001fb53e001a48b828e8cc9ff9
MaryKaya <sceran_biyoloji@hotmail.com>
2019-09-17 20:24:39 -0500

    This is the new code

---
+++ src/Arrays/ArraysOddNumber.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArraysOddNumber {
+    public static void main(String[] args) {
+        int numbers[]={39, 87,91,123,876,};
+        Arrays.sort(numbers);
+        for(int i:numbers){
+            System.out.println(i);
+
+        }
+    }
+}
